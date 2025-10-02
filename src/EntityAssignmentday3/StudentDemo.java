f50bb525f5a1fbda686f405e1e13fe864ba3868a
nandhini <nandhini@LAPTOP-7UKUPIIS>
2025-10-03 05:00:20 +0530

    This is a basic java project

---
+++ src/EntityAssignmentday3/StudentDemo.java
@@ -0,0 +1,16 @@
+package EntityAssignmentday3;
+import EntityAssignmentday3.Student;
+
+public class StudentDemo {
+	 Student s1 = new Student();
+	 s1.setId(101);
+	 s1.setName("Raja");
+	 s1.setCity("Villupuram);
+	 System.out.println(s1);	
+    Student s2 = new Student();
+	 s2.setId(102);
+	 s2.setName("Arjun");
+	 s1.setCity("Cuddalore");
+	 System.out.println(s2);
+
+}
