aa56d3f1142d7a5133d6f7986cfe8b851a5fc45e
nandhini <nandhini@LAPTOP-7UKUPIIS>
2025-10-02 19:00:45 +0530

    This is a basic java program

---
+++ src/day4oops_polymorphism/OverridingShape.java
@@ -0,0 +1,18 @@
+
+package day4oops_polymorphsim.overriding.v1;
+
+
+public class Shape {
+
+	// Member function
+	public void draw() {
+		System.out.println("Drawing a generic shape");
+	}
+
+	public void erase() {
+		System.out.println("Erasing a generic shape");
+	}
+}
+
+
+
