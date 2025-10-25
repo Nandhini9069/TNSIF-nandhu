61cead132fc4940b4842101ad5320feab5836bfb
nandhini <nandhini@LAPTOP-7UKUPIIS>
2025-10-25 05:43:27 +0530

    This is a Basic Program

---
+++ Tnsif_Java_Nandhini/src/day8lambdaexpression/Operations.java
@@ -0,0 +1,6 @@
+package day8lambdaexpression;
+
+@FunctionalInterface
+public interface Operations {
+	float performArithmetic(int a, int b);
+}
---
+++ Tnsif_Java_Nandhini/src/day8lambdaexpression/Palindrome.java
@@ -0,0 +1,6 @@
+package day8lambdaexpression;
+
+public interface Palindrome<T> {
+	boolean checkPalindrome(T data);
+}
+
